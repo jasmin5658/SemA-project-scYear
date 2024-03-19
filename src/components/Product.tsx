b5032ee25f04efda484f96edcf318a5e6e2aab16
jasmin5658 <kouzlyjasmin@gmail.com>
2024-03-19 18:43:27 +0200

    Update Product.tsx

--- src/components/Product.tsx
+++ src/components/Product.tsx
@@ -9,7 +9,7 @@ export default function Product({ product }: ProductComp) {
       <h1>{product.name}</h1>
       <p>{product.id}</p>
       <p>{product.shortDesc}</p>
-      <p>{product.longDesc}</p>
+      {/* <p>{product.longDesc}</p> */}
       <Image src={product.imag} alt={product.name} width={200} height={200} />
       <p>{product.minQty}</p>
       <p>{product.currQty}</p>
