1194fed6878c44bf6faac656e03507fd653909c7
jasmin5658 <kouzlyjasmin@gmail.com>
2024-05-09 13:50:53 +0300

    t

--- cypress/e2e/addProducts.cy.js
+++ cypress/e2e/addProducts.cy.js
@@ -1,8 +1,6 @@
-describe('Remove', () => {
-    it('allow you to remove a product from the cart', () => {
-      cy.visit('http://10.100.102.14:5173/')
-      cy.contains('').click();
-      cy.contains('Add To Cart').click();
-      cy.contains('Remove').click();
-    })
-  })
+describe('Remove Product from Cart', () => {
+  it('allows a user to remove a product from the cart', () => {
+    // Step 1: Navigate to the Store Page
+    cy.visit('http://10.100.102.14:5173/store')
+  })
+})
